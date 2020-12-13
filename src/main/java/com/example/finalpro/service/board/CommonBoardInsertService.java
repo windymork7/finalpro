2ae574be644f5818811886d1af7606052adce571
MyungJa <windymork7@naver.com>
2020-12-13 22:17:22 +0900

    명자 20.12.13 pm 10:17

--- src/main/java/com/example/finalpro/service/board/CommonBoardInsertService.java
+++ src/main/java/com/example/finalpro/service/board/CommonBoardInsertService.java
@@ -3,6 +3,8 @@ package com.example.finalpro.service.board;
 import com.example.finalpro.vo.QboardVO;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface CommonBoardInsertService {
-    public void qBoardInsert(MultipartFile q_file, QboardVO qboardVO);
+    public void qBoardInsert(MultipartFile q_file, QboardVO qboardVO, HttpServletRequest request);
 }
